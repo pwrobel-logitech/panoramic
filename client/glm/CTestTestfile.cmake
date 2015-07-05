a5c4031481ca5f0eafb194a40e42e8a545a214f8
pwrobel <wrobel1989@gmail.com>
2015-07-05 22:05:00 +0200

    Add glm functionality for the matrix calculations - this library is on the MIT licence

---
+++ client/glm/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pwrobel/programing/panoramic/client/glm/glm
+# Build directory: /home/pwrobel/programing/panoramic/client/glm/glm
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
--- client/glrenderer.h
+++ client/glrenderer.h
@@ -5,14 +5,36 @@
 #include <string>
 #include <iostream>
 #include <cmath>
+#include "glm/glm.hpp"
 
 namespace glrenderer {
 
 //OpenGL context
 extern SDL_GLContext gContext;
 
+//world rendering info
+struct worldinfo{
+	int resX;
+	int resY;
+	double fovX;// total angle in radians
+	double horizontal_angular_overlap; //in radians, =0 by default
+	glm::mat4 projection;
+	glm::mat4 view_central_cam;
+};
+
+//minimal texture info
+struct texinfo{
+	int ntexX;
+	int ntexY;
+};
+
+extern worldinfo myworld;
+extern texinfo texdata;
+
 bool initGL(int sx, int sy);
 
 void renderGL();
+
+void setup_projection();
 
 }
